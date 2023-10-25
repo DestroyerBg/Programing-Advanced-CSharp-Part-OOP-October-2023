8ee836f765ecc8e55407c611a07546830dfd09f5
DestroyerBg <gabriel-kulev@abv.bg>
2023-10-25 15:13:32 +0300

    All Exercices

---
+++ 1.Inheritance-Lab/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (this.Any())
+            {
+                return true;
+            }
+            return false;
+        }
+        public Stack<string> AddRange(List<string> range)
+        {
+            foreach (var item in range)
+            {
+                this.Push(item);
+            }
+            return this;
+        }
+
+    }
+}
