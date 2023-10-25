ca29f663b951265301741e7be5ddc9f32ddd75a8
DestroyerBg <gabriel-kulev@abv.bg>
2023-10-25 17:15:58 +0300

    All Exercices

---
+++ 2.Inheritance-Exercise/01.Person/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (age < 0)
+                {
+                    throw new InvalidOperationException("Invalid operation");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
