624c102d4cc755c0ea3fb43398e2c6bcc68d165c
DestroyerBg <gabriel-kulev@abv.bg>
2023-10-25 22:53:51 +0300

    All Exercices

--- 2.Inheritance-Exercise/03.PlayersAndMonsters/StartUp.cs
+++ 2.Inheritance-Exercise/03.PlayersAndMonsters/StartUp.cs
@@ -1,10 +1,15 @@
-﻿namespace PlayersAndMonsters
+﻿using System;
+
+namespace PlayersAndMonsters
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-
+            string username = Console.ReadLine();
+            int level = int.Parse(Console.ReadLine());
+            DarkKnight darkKnight = new DarkKnight(username, level);
+            Console.WriteLine(darkKnight.ToString());
         }
     }
 }
