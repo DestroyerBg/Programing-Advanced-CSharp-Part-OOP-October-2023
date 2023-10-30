cd68583c338059d0709cc53839571714bef9f77d
DestroyerBg <gabriel-kulev@abv.bg>
2023-10-30 18:42:59 +0200

    All Exercices

---
+++ 4.Encapsulation-Exercise/03.ShoppingSpree/Person.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> products;
+
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            products = new List<Product>();
+        }
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                name = value;
+            }
+
+        }
+
+        public decimal Money
+        {
+            get => money;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value;
+            }
+
+        }
+
+        public IReadOnlyCollection<Product> Products
+        {
+            get => products.AsReadOnly();
+        }
+
+        public string BuyProduct(Product product)
+        {
+            if (Money >= product.Cost)
+            {
+                products.Add(product);
+                Money-=product.Cost;
+                return $"{Name} bought {product.Name}";
+            }
+
+            return $"{Name} can't afford {product.Name}";
+        }
+
+        public override string ToString()
+        {
+            if (products.Any())
+            {
+                return $"{Name} - {string.Join(", ",products.Select(p=>p.Name))}";
+            }
+
+            return $"{Name} - Nothing bought";
+        }
+    }
+}
--- 4.Encapsulation-Exercise/03.ShoppingSpree/Program.cs
+++ 4.Encapsulation-Exercise/03.ShoppingSpree/Program.cs
@@ -1,1 +1,51 @@
-﻿
+﻿using _03.ShoppingSpree;
+
+var persons = new List<Person>();
+var productPrices = new List<Product>();
+try
+{
+    string[] people = Console.ReadLine()
+        .Split(";", StringSplitOptions.RemoveEmptyEntries);
+    for (int i = 0; i < people.Length; i++)
+    {
+        string[] currPerson = people[i].Split('=', StringSplitOptions.RemoveEmptyEntries);
+        string name = currPerson[0];
+        decimal money = decimal.Parse(currPerson[1]);
+        Person person = new Person(name, money);
+        persons.Add(person);
+    }
+    string[] products = Console.ReadLine()
+        .Split(";", StringSplitOptions.RemoveEmptyEntries);
+    for (int i = 0; i < products.Length; i++)
+    {
+        string[] currProduct = products[i].Split('=', StringSplitOptions.RemoveEmptyEntries);
+        string productName = currProduct[0];
+        decimal price = decimal.Parse(currProduct[1]);
+        var product = new Product(productName, price);
+        productPrices.Add(product);
+    }
+
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+    Environment.Exit(0);
+}
+string input = string.Empty;
+while ((input = Console.ReadLine()) != "END")
+{
+    string[] data = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string personName = data[0];
+    string productName = data[1];
+    var product = productPrices.FirstOrDefault(p => p.Name == productName);
+    var person = persons.FirstOrDefault(p => p.Name == personName);
+    if (person is not null && product is not null)
+    {
+        Console.WriteLine(persons.Find(p => p.Name == personName).BuyProduct(product));
+    }
+}
+
+foreach (var person in persons)
+{
+    Console.WriteLine(person.ToString());
+}
