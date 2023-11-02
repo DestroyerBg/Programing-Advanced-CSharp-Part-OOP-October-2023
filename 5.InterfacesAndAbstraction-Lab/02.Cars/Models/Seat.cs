e225ffa91532164dd1e523f1010c57099d9686ae
DestroyerBg <gabriel-kulev@abv.bg>
2023-11-02 18:07:02 +0200

    All Exercices

---
+++ 5.InterfacesAndAbstraction-Lab/02.Cars/Models/Seat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Models
+{
+    public class Seat : ICar
+    {
+        public Seat(string name, string color)
+        {
+            Name = name;
+            Color = color;
+        }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Start() => $"Engine start";
+
+
+        public string Stop() => $"Breaaak!";
+        
+
+        public override string ToString()
+        { return $"{Color} Seat {Name}{Environment.NewLine}" +
+                 $"{Start()}{Environment.NewLine}"+
+                 $"{Stop()}";
+        }
+    }
+}
---
+++ 5.InterfacesAndAbstraction-Lab/02.Cars/Models/Tesla.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Models
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string name, string color, int battery)
+        {
+            Name = name;
+            Color = color;
+            Battery = battery;
+        }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+        public string Start() => "Engine start";
+        public string Stop() => $"Breaaak!";
+
+        public override string ToString()
+        {
+            return $"{Color} Tesla {Name} with {Battery} Batteries{Environment.NewLine}" +
+                   $"{Start()}{Environment.NewLine}" +
+                   $"{Stop()}";
+        }
+    }
+}
